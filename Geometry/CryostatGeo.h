2f333cf65bca8d15eb89712e19fee22da1b29f7d
Gianluca Petrillo <petrillo@fnal.gov>
2015-05-06 12:55:28 -0500

    Sorted the declaration of geo::CryostatGeo and added a little documentation

--- Geometry/CryostatGeo.h
+++ Geometry/CryostatGeo.h
@@ -25,33 +25,42 @@ namespace geo {
   class OpDetGeo;
 
   //......................................................................
-  /// Geometry information for a single tpc
+  /// Geometry information for a single cryostat
   class CryostatGeo {
   public:
-    // Construct a representation of a single cryostat of the detector
+    
+    /// Construct a representation of a single cryostat of the detector
     CryostatGeo(std::vector<const TGeoNode*>& path, int depth);
+    
+    /// Destructor: destructs
     ~CryostatGeo();
 
-    // Method to sort TPCGeo objects
-    void              SortSubVolumes(geo::GeoObjectSorter const& sorter);
-
-    // Number of tpcs in this cryostat
+    
+    /// @{
+    /// @name Cryostat geometry information
+    
+    /// Half width of the cryostat
+    double            HalfWidth()                               const;
+    /// Half height of the cryostat
+    double            HalfHeight()                              const;
+    /// Length of the cryostat
+    double            Length()                                  const;
+    /// Mass of the cryostat
+    double            Mass()                                    const { return fVolume->Weight(); }
+    /// Pointer to ROOT's volume descriptor
+    const TGeoVolume* Volume()                                  const { return fVolume;           }
+    
+    /// @}
+    
+    
+    /// @{
+    /// @name TPC access
+    
+    /// Number of TPCs in this cryostat
     unsigned int      NTPC()                                    const { return fTPCs.size();      }
 
-    // Number of optical detectors in this tpc
-    unsigned int      NOpDet()                                  const { return fOpDets.size();    }
-
-    // Return the itpc'th tpc in the cryostat.
+    /// Return the itpc'th TPC in the cryostat.
     const TPCGeo&     TPC(unsigned int itpc)                    const;
-
-    // Return the iopdet'th tpc in the cryostat.
-    const OpDetGeo&   OpDet(unsigned int iopdet)                const;
-
-    double            HalfWidth()                               const; // half width of the cryostat
-    double            HalfHeight()                              const; // half height of the cryostat
-    double            Length()                                  const; // length of the cryostat
-    double            Mass()                                    const { return fVolume->Weight(); }
-    const TGeoVolume* Volume()                                  const { return fVolume;           }
 
     /**
      * @brief Returns the index of the TPC at specified location
@@ -62,36 +71,57 @@ namespace geo {
     unsigned int FindTPCAtPosition(double const worldLoc[3],
                                    double const wiggle) const;
     
+    /// Return the TPCGeo object containing the world position worldLoc
+    /// @todo What if there is none?
     const TPCGeo&     PositionToTPC(double const  worldLoc[3],
 				    unsigned int &tpc,
-				    double const &wiggle)       const; // return the TPCGeo object 
-                                                                       // containing the
-                                                                       // world position worldLoc
+				    double const &wiggle)       const;
+    /// @}
+    
+    
+    /// @{
+    /// @name Optical detector access
+    
+    /// Number of optical detectors in this TPC
+    unsigned int      NOpDet()                                  const { return fOpDets.size();    }
+    
+    /// Return the iopdet'th optical detector in the cryostat
+    const OpDetGeo&   OpDet(unsigned int iopdet)                const;
 
-    // Transform point from local plane frame to world frame
-    void LocalToWorld(const double* tpc, double* world)         const;
-
-    // Transform direction vector from local to world
-    void LocalToWorldVect(const double* tpc, double* world)     const;
-
-    // Again, with TVectors
-    const TVector3 LocalToWorld( const TVector3& local )        const;
-
-    // Transform point from world frame to local tpc frame
-    void WorldToLocal(const double* world, double* tpc)         const;
-
-    // Transform direction vector from world to local
-    void WorldToLocalVect(const double* world, double* tpc)     const;
-
-    // Again, with TVectors
-    const TVector3 WorldToLocal( const TVector3& world )        const;
-
-    // Get name of opdet geometry element
-    std::string  OpDetGeoName()                                 const { return fOpDetGeoName; }
-
-    // Find the nearest opdet to point in this cryostat
+    /// Find the nearest opdet to point in this cryostat
     unsigned int GetClosestOpDet(double * xyz)                  const;
     
+    /// Get name of opdet geometry element
+    std::string  OpDetGeoName()                                 const { return fOpDetGeoName; }
+
+    /// @}
+
+    /// @{
+    /// @name Coordinate transformation
+    
+    //@{
+    /// Transform point from local plane frame to world frame
+    void LocalToWorld(const double* tpc, double* world)         const;
+    const TVector3 LocalToWorld( const TVector3& local )        const;
+    //@}
+    
+    /// Transform direction vector from local to world
+    void LocalToWorldVect(const double* tpc, double* world)     const;
+    
+    //@{
+    /// Transform point from world frame to local tpc frame
+    void WorldToLocal(const double* world, double* tpc)         const;
+    const TVector3 WorldToLocal( const TVector3& world )        const;
+    //@}
+    
+    // Transform direction vector from world to local
+    void WorldToLocalVect(const double* world, double* tpc)     const;
+    
+    ///@}
+    
+    /// Method to sort TPCGeo objects
+    void              SortSubVolumes(geo::GeoObjectSorter const& sorter);
+
     
   private:
 
