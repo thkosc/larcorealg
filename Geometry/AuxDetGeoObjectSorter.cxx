9004003f1f63a6bbc56b5c74b537174926a897ab
Brian Rebel <brebel@fnal.gov>
2015-07-15 11:17:17 -0500

    first commit of new files related to AuxDetGeometry navigation.  The objects defined by these files are analogs to the traditional Geometry navigation objects.

---
+++ Geometry/AuxDetGeoObjectSorter.cxx
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////
+/// \file  GeoObjectSorter.cxx
+/// \brief Interface to algorithm class for sorting geo::XXXGeo objects
+///
+/// \version $Id:  $
+/// \author  brebel@fnal.gov
+////////////////////////////////////////////////////////////////////////
+
+#include "Geo/AuxDetGeoObjectSorter.h"
+
+namespace geo{
+
+
+  //----------------------------------------------------------------------------
+  AuxDetGeoObjectSorter::AuxDetGeoObjectSorter()
+  {
+  }
+
+  //----------------------------------------------------------------------------
+  AuxDetGeoObjectSorter::~AuxDetGeoObjectSorter()
+  {
+  }
+
+}
