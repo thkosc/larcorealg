685424771c6e2e647e9da010b8598e2c82acd517
Gianluca Petrillo <petrillo@fnal.gov>
2016-07-27 22:21:54 -0500

    Added lar::providersFrom() to ServiceUtil.h
    This function allows one-step creation of a service provider pack, making the initialisation of a provider shorter.

--- larcore/CoreUtils/ProviderPack.h
+++ larcore/CoreUtils/ProviderPack.h
@@ -138,6 +138,33 @@ namespace lar {
           (*this, ProviderPack<OtherProviders...>(providers...));
       }
 
+    /**
+     * @brief Constructor: picks the providers from a pack plus specified ones
+     * @tparam FromPack parameter pack to start from
+     * @tparam OtherProviders list of the type of providers offered
+     * @param fromPack providers to be picked
+     * @param providers all the remaining providers needed (or more)
+     * @see expandProviderPack()
+     * 
+     * This constructor will pick all the providers from the specified pack,
+     * and the ones from the other providers.
+     * This constructor can be used to "expand" from another provider:
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     *     A a;
+     *     B b;
+     *     C c;
+     *     D d;
+     *     ProviderPack<A, D> pack(&a, &d);
+     *     ProviderPack<A, B, C, D> largerPack(pack, &c, &b);
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+    template<typename... PackProviders, typename... OtherProviders>
+    ProviderPack(
+      ProviderPack<PackProviders...> const& fromPack,
+      OtherProviders const*... providers
+      );
+    
+    
     /// Returns the provider with the specified type
     template <typename Provider>
     Provider const* get() const
@@ -163,6 +190,16 @@ namespace lar {
         return details::index_with_type_impl<Provider, Providers...>::value
           < sizeof...(Providers);
       } // has<>()
+    
+    
+    /// Returns whether other provider pack has all the same providers as this
+    template <typename... OtherProviders>
+    bool operator== (ProviderPack<OtherProviders...> const& other) const;
+    
+    /// Returns whether other provider pack and this have different providers
+    template <typename... OtherProviders>
+    bool operator!= (ProviderPack<OtherProviders...> const& other) const;
+    
     
       private:
     
@@ -191,6 +228,34 @@ namespace lar {
     { return ProviderPack<Providers...>(providers...); }
   
   
+  /**
+   * @brief Function to create a ProviderPack by adding to another
+   * @tparam PackProviders types of the providers in the original parameter pack
+   * @tparam MoreProviders types of the providers to be added
+   * @param pack parameter pack with the first providers
+   * @param providers constant pointers to the other providers to be added
+   * @return a ProviderPack object containing all the specified providers
+   *
+   * This is an convenience function to reduce the typing needed to instantiate
+   * a ProviderPack. Use it like:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   *     A a;
+   *     B b;
+   *     C c;
+   *     D d;
+   *     auto pack = makeProviderPack(&a, &d);
+   *     auto largerPack = expandProviderPack(pack, &c, &b);
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * creates a `ProviderPack<A, D, C, B>` including all the four objects.
+   */
+  template <typename... PackProviders, typename... MoreProviders>
+  ProviderPack<PackProviders..., MoreProviders...> expandProviderPack(
+    ProviderPack<PackProviders...> const& pack,
+    MoreProviders const* ...providers
+    )
+    { return { pack, providers... }; }
+  
+  
 } // namespace lar
 
 
@@ -201,19 +266,78 @@ namespace lar {
   namespace details {
     
     //--------------------------------------------------------------------------
-    //--- has_duplicate_types
+    //--- has_type, has_duplicate_types, are_same_types
     //---
+    template <typename Target, typename... Types>
+    struct has_type:
+      public std::integral_constant
+        <bool, index_with_type_impl<Target, Types...>::value < sizeof...(Types)>
+      {};
+    
+    
+    //--------------------------------------------------------------------------
     template <typename Key, typename... Types>
     struct has_duplicate_types<Key, Types...>:
-      public std::integral_constant<
-        bool,
-        index_with_type_impl<Key, Types...>::value < sizeof...(Types)
-          || has_duplicate_types<Types...>::value
-        >
+      public std::integral_constant
+        <bool, has_type<Key, Types...>() || has_duplicate_types<Types...>()>
       {};
     
     template <>
     struct has_duplicate_types<>: public std::false_type {};
+    
+    
+    //--------------------------------------------------------------------------
+    template <typename... Types>
+    struct are_types_contained;
+    
+    template <typename... Types>
+    struct are_same_types {
+      
+      template <typename... AsTypes>
+      static constexpr bool as()
+        {
+          return (sizeof...(Types) == sizeof...(AsTypes))
+            && are_types_contained<Types...>::template in<AsTypes...>();
+        }
+      
+    }; // are_same_types
+    
+    
+    template <typename First, typename... OtherTypes>
+    struct are_types_contained<First, OtherTypes...> {
+      template <typename... AsTypes>
+      static constexpr bool in()
+        {
+          return are_types_contained<OtherTypes...>::template in<AsTypes...>()
+            && has_type<First, AsTypes...>();
+        }
+    };
+    
+    template <typename First>
+    struct are_types_contained<First> {
+      template <typename... AsTypes>
+      static constexpr bool in()
+        { return has_type<First, AsTypes...>(); }
+    };
+
+    
+    template <typename T>
+    struct is_provider_pack: public std::false_type {};
+    
+    template <typename... Providers>
+    struct is_provider_pack<ProviderPack<Providers...>>: public std::true_type
+      {};
+    
+    
+    template <typename APack, typename BPack>
+    struct have_same_provider_types: public std::false_type {};
+    
+    template <typename... AProviders, typename... BProviders>
+    struct have_same_provider_types
+      <ProviderPack<AProviders...>, ProviderPack<BProviders...>>
+      : public std::integral_constant
+        <bool, are_same_types<AProviders...>::template as<BProviders...>()>
+      {};
     
     
     //--------------------------------------------------------------------------
@@ -274,6 +398,51 @@ namespace lar {
     };
 
     //--------------------------------------------------------------------------
+    //--- Compare
+    //---
+    template <typename Provider, typename APack, typename BPack>
+    bool haveSameProvider(APack const& a, BPack const& b) {
+      static_assert(is_provider_pack<APack>() && is_provider_pack<BPack>(),
+        "This class needs two ProviderPack template types.");
+      return a.template get<Provider>() == b.template get<Provider>();
+    } // haveSameProvider()
+    
+    
+    template <typename APack, typename BPack>
+    struct ProviderPackComparerBase {
+      
+      static_assert(have_same_provider_types<APack, BPack>(),
+        "The specified provider packs have different types.");
+      
+    }; // ProviderPackComparerBase
+    
+    
+    template <typename APack, typename BPack, typename... Providers>
+    struct ProviderPackComparer;
+    
+    template
+      <typename APack, typename BPack, typename First, typename... Others>
+    struct ProviderPackComparer<APack, BPack, First, Others...>
+      : ProviderPackComparerBase<APack, BPack>
+    {
+      static bool compare (APack const& a, BPack const& b)
+        {
+          return haveSameProvider<First>(a, b)
+            && ProviderPackComparer<APack, BPack, Others...>::compare(a, b);
+        }
+    }; // ProviderPackComparer<APack, BPack, First, Others...>
+    
+    template
+      <typename APack, typename BPack, typename First>
+    struct ProviderPackComparer<APack, BPack, First>
+      : ProviderPackComparerBase<APack, BPack>
+    {
+      static bool compare (APack const& a, BPack const& b)
+        { return haveSameProvider<First>(a, b); }
+    }; // ProviderPackComparer<APack, BPack, First>
+    
+    
+    //--------------------------------------------------------------------------
 
   } // namespace details
   
@@ -281,6 +450,52 @@ namespace lar {
   //----------------------------------------------------------------------------
   //--- ProviderPack
   //---
+  
+  template <typename... Providers>
+  template<typename... PackProviders, typename... OtherProviders>
+  ProviderPack<Providers...>::ProviderPack(
+    ProviderPack<PackProviders...> const& fromPack,
+    OtherProviders const*... providers
+    )
+  {
+    
+    // verify that the list of providers in argument is the exact one we need
+    static_assert(
+      details::are_same_types<Providers...>
+        ::template as<PackProviders..., OtherProviders...>(),
+      "The providers types in the arguments do not match the ones needed."
+      );
+    
+    // copy all the providers from the provider pack
+    details::SetFrom
+      <this_type, ProviderPack<PackProviders...>, PackProviders...>
+      (*this, fromPack);
+    
+    // put the other providers in a temporary parameter pack, and copy it
+    // (this is convenience, a direct implementation would be probably better)
+    details::SetFrom
+      <this_type, ProviderPack<OtherProviders...>, OtherProviders...>
+      (*this, makeProviderPack(providers...));
+    
+  } // ProviderPack<Providers...>::ProviderPack(ProviderPack, OtherProviders...)
+  
+  
+  template <typename... Providers>
+  template <typename... OtherProviders>
+  bool ProviderPack<Providers...>::operator==
+    (ProviderPack<OtherProviders...> const& other) const
+  {
+    return details::ProviderPackComparer<
+      ProviderPack<Providers...>, ProviderPack<OtherProviders...>, Providers...
+      >::compare(*this, other);
+  }
+  
+  template <typename... Providers>
+  template <typename... OtherProviders>
+  bool ProviderPack<Providers...>::operator!=
+    (ProviderPack<OtherProviders...> const& other) const
+    { return !(*this == other); }
+  
  
   //----------------------------------------------------------------------------
   
