1d274688afbe50c67abc1b0ecb1fb059979e39ab
Gianluca Petrillo <petrillo@fnal.gov>
2017-03-01 20:02:38 -0600

    Extended DumpUtils.h to print arrays.
    Also vector3D() now supports printing vectors from pointers and C-style
    arrays.

--- larcore/CoreUtils/DumpUtils.h
+++ larcore/CoreUtils/DumpUtils.h
@@ -18,6 +18,73 @@ namespace lar {
   /// Namespace for LArSoft dumping utilities.
   namespace dump {
     
+    namespace details {
+      
+      /// Inserts `n` of elements of `a` in the specified stream.
+      template <typename Stream, typename Array>
+      void dumpArray(Stream&& out, Array&& a, size_t n) {
+        out << "{";
+        if (n == 0) { out << "}"; return; }
+        out << " " << a[0];
+        for (size_t i = 1; i < n; ++i) out << "; " << a[i];
+        out << " }";
+      } // dumpArray()
+      
+    } // namespace details
+    
+    /**
+     * @brief Dumps the first N elements of an array.
+     * @tparam Array the array type (supporting indexing operator)
+     * @tparam NPrint the number of elements to be printed
+     * 
+     * The dump is producer by the call operator, in the form
+     *     
+     *     { a1, a2, ... aN }
+     *     
+     * 
+     */
+    template <typename Array>
+    struct ArrayDumper {
+      using Array_t = Array;
+      using This_t = ArrayDumper<Array_t>;
+      
+      Array_t const& a; ///< A reference to the array to be printed.
+      size_t n; ///< Number of elements to be printed.
+      
+      ArrayDumper(Array_t const& a, size_t n): a(a), n(n) {}
+      
+      // constructors ahead
+      ArrayDumper(This_t const& from) = default;
+      ArrayDumper(This_t&& from) = default;
+      ArrayDumper& operator=(This_t const& from) = delete;
+      ArrayDumper& operator=(This_t&& from) = delete;
+      
+      /// Inserts the content of the referenced array into the specified stream.
+      template <typename Stream>
+      void operator() (Stream&& out) const
+        { details::dumpArray(std::forward<Stream>(out), a, n); }
+      
+    }; // struct ArrayDumper
+    
+    
+    template <typename T>
+    struct ArrayDumper<T*> {
+      using Array_t = T*;
+      using This_t = ArrayDumper<Array_t>;
+      
+      Array_t a; ///< A reference to the array to be printed.
+      size_t n; ///< Number of elements to be printed.
+      
+      ArrayDumper(Array_t a, size_t n): a(a), n(n) {}
+      
+      /// Inserts the content of the referenced array into the specified stream.
+      template <typename Stream>
+      void operator() (Stream&& out) const
+        { details::dumpArray(std::forward<Stream>(out), a, n); }
+      
+    }; // struct ArrayDumper<T*>
+    
+    
     /**
      * @brief Manipulator managing the dump of the vector content into a stream.
      * @tparam Vector the type of vector to be dumped
@@ -25,6 +92,10 @@ namespace lar {
      * The manipulator is constructed with the object that needs to be dumped.
      * This implementation requires a vector of at least 3D with methods `X()`,
      * `Y()` and `Z()`.
+     * The dump is producer by the call operator, in the form
+     *     
+     *     { vx, vy, vz }
+     *     
      * 
      * An overloaded insertion operator will call the `operator()` method of
      * the manipulator to perform the insertion.
@@ -62,11 +133,11 @@ namespace lar {
     template <typename Vector>
     struct VectorDumper {
       using Vector_t = Vector;
-      using This_t = VectorDumper<Vector>;
+      using This_t = VectorDumper<Vector_t>;
       
       Vector_t const& v; ///< A reference to the vector to be printed.
       
-      VectorDumper(Vector_t const& v): v(v) {}
+      explicit VectorDumper(Vector_t const& v): v(v) {}
       
       // constructors ahead
       VectorDumper(This_t const& from) = default;
@@ -80,6 +151,56 @@ namespace lar {
         { out << "{ " << v.X() << "; " << v.Y() << "; " << v.Z() << " }"; }
       
     }; // struct VectorDumper<>
+    
+    
+    // Specialization for bare pointers.
+    template <typename T>
+    struct VectorDumper<T*>: public ArrayDumper<T const*> {
+      explicit VectorDumper(T* v): ArrayDumper<T const*>(v, 3U) {}
+    }; // VectorDumper<T*>
+    
+    // Specialization for C-style arrays.
+    template <typename T>
+    struct VectorDumper<T[3]>: public ArrayDumper<T const*> {
+      explicit VectorDumper(T const* v): ArrayDumper<T const*>(v, 3U) {}
+    }; // VectorDumper<T*>
+    
+    
+    
+    /**
+     * @brief Returns a manipulator which will print the specified array.
+     * @tparam NPrint the number of entries to be printed
+     * @tparam Array the type of 3D vector to be printed
+     * @param a the array to be printed
+     * @return a manipulator
+     * 
+     * Example of usage:
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+     * double const data[5] = { 1., 2., 3., 4., 6. };
+     * std::cout << "Position: " << lar::dump::array<5>(data) << std::endl;
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * will produce an output like:
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * Position: { 1; 2; 3; 4; 6 }
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * 
+     * Addition of specific classes can be achieved via specialization of
+     * the class `ArrayDumper`.
+     * 
+     * 
+     * Requirements
+     * -------------
+     * 
+     * * the type `Array` is required to provide constant index operator
+     *   returning the array element at the specified index
+     * * the value type returned by the aforementioned `Array` method must have
+     *   its insertion operator into a stream defined
+     * * the array `a` must not fall out of scope until the insertion into
+     *   the stream happens
+     * 
+     */
+    template <size_t N, typename Array>
+    auto array(Array const& a) { return ArrayDumper<Array>(a, N); }
     
     
     
@@ -116,6 +237,42 @@ namespace lar {
      */
     template <typename Vector3D>
     auto vector3D(Vector3D const& v) { return VectorDumper<Vector3D>(v); }
+    
+    
+    
+    /**
+     * @brief Dumps the array contained in the manipulator into a stream.
+     * @tparam Stream the type of output stream
+     * @tparam Array the type of array to be printed
+     * @tparam NPrint the number of element of the array to be printed
+     * @param out the output stream
+     * @param manip the manipulator containing the array to be printed
+     * @return a reference to the output stream
+     * @see ArrayDumper
+     * 
+     * Example of usage:
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+     * double const data[5] = { 1., 2., 3., 4., 6. };
+     * std::cout << "Position: " << lar::dump::array<5>(data) << std::endl;
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * will produce an output like:
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * Position: { 1, 2, 3, 4, 5 }
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * 
+     * 
+     * Requirements
+     * -------------
+     * 
+     * * the type `Vector3D` is required to provide constant accessor methods
+     *   returning the vector components, called `X()`, `Y()` and `Z()`
+     * * the values returned by the aforementioned `Vector3D` methods must have
+     *   their insertion operator into Stream defined
+     * 
+     */
+    template <typename Stream, typename Array>
+    Stream& operator<< (Stream&& out, ArrayDumper<Array>&& manip)
+      { manip(std::forward<Stream>(out)); return out; }
     
     
     /**
--- test/CoreUtils/DumpUtils_test.cc
+++ test/CoreUtils/DumpUtils_test.cc
@@ -8,7 +8,7 @@
  */
 
 // Boost libraries
-#define BOOST_TEST_MODULE ( RealComparisons_test )
+#define BOOST_TEST_MODULE DumpUtils_test
 #include <cetlib/quiet_unit_test.hpp> // BOOST_AUTO_TEST_CASE()
 #include <boost/test/test_tools.hpp> // BOOST_CHECK_EQUAL()
 
@@ -21,7 +21,141 @@
 // C/C++ standard libraries
 #include <iostream>
 #include <string>
+#include <vector>
+#include <array>
 #include <sstream>
+
+
+//------------------------------------------------------------------------------
+void ArrayTest_STLarray() {
+  std::ostringstream sstr;
+  std::array<float, 5> const array = { 1., 2., 3., 4., 6. };
+  sstr << lar::dump::array<5>(array);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 3; 4; 6 }");
+}
+
+void ArrayTest_STLvector() {
+  std::ostringstream sstr;
+  std::vector<float> const array = { 1., 2., 3., 4., 6. };
+  sstr << lar::dump::array<5>(array);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 3; 4; 6 }");
+}
+
+void ArrayTest_carray() {
+  std::ostringstream sstr;
+  float const array[5] = { 1., 2., 3., 4., 6. };
+  sstr << lar::dump::array<5>(array);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 3; 4; 6 }");
+}
+
+void ArrayTest_cptr() {
+  std::ostringstream sstr;
+  float const array[5] = { 1., 2., 3., 4., 6. };
+  float const* ptr = array;
+  sstr << lar::dump::array<5>(ptr);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 3; 4; 6 }");
+}
+
+void ArrayTest_array() {
+  std::ostringstream sstr;
+  double array[5] = { 1., 2., 3., 4., 6. };
+  sstr << lar::dump::array<5>(array);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 3; 4; 6 }");
+}
+
+void ArrayTest_ptr() {
+  std::ostringstream sstr;
+  double array[5] = { 1., 2., 3., 4., 6. };
+  double* ptr = array;
+  sstr << lar::dump::array<5>(ptr);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 3; 4; 6 }");
+}
+
+void ArrayTest() {
+  
+  ArrayTest_STLarray();
+  ArrayTest_STLvector();
+  ArrayTest_array();
+  ArrayTest_carray();
+  ArrayTest_ptr();
+  ArrayTest_cptr();
+  
+} // Vector3Dtest()
+
+
+//------------------------------------------------------------------------------
+void ArrayDocumentationTest() {
+  
+  std::ostringstream sstr;
+  
+  /* This is the code as in the documentation:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * double const data[5] = { 1., 2., 3., 4., 6. };
+   * std::cout << "Position: " << lar::dump::array<5>(data) << std::endl;
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * which is promised to return:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * Position: { 1; 2; 3; 4; 6 }
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   */
+
+  double const data[5] = { 1., 2., 3., 4., 6. };
+  std::cout << "Position: " << lar::dump::array<5>(data) << std::endl;
+
+  sstr << lar::dump::array<5>(data);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 3; 4; 6 }");
+  
+} // ArrayDocumentationTest()
+
+
+//------------------------------------------------------------------------------
+void Vector3Dtest_TVector3() {
+  std::ostringstream sstr;
+  TVector3 pos( 1., 2., 4. );
+  sstr << lar::dump::vector3D(pos);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 4 }");
+}
+
+void Vector3Dtest_carray() {
+  std::ostringstream sstr;
+  float const array[3] = { 1., 2., 4. };
+  sstr << lar::dump::vector3D(array);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 4 }");
+}
+
+void Vector3Dtest_cptr() {
+  std::ostringstream sstr;
+  float const array[3] = { 1., 2., 4. };
+  float const* ptr = array;
+  sstr << lar::dump::vector3D(ptr);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 4 }");
+}
+
+void Vector3Dtest_array() {
+  std::ostringstream sstr;
+  double array[3] = { 1., 2., 4. };
+  sstr << lar::dump::vector3D(array);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 4 }");
+}
+
+void Vector3Dtest_ptr() {
+  std::ostringstream sstr;
+  double array[3] = { 1., 2., 4. };
+  double* ptr = array;
+  sstr << lar::dump::vector3D(ptr);
+  BOOST_CHECK_EQUAL(sstr.str(), "{ 1; 2; 4 }");
+}
+
+
+void Vector3Dtest() {
+  
+  Vector3Dtest_TVector3();
+  Vector3Dtest_array();
+  Vector3Dtest_carray();
+  Vector3Dtest_ptr();
+  Vector3Dtest_cptr();
+  
+} // Vector3Dtest()
 
 
 //------------------------------------------------------------------------------
@@ -91,7 +225,18 @@ void Vector3DspecializationTest() {
 
 
 //------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(test_DumpArrays) {
+  
+  ArrayTest();
+  
+  ArrayDocumentationTest();
+  
+} // BOOST_AUTO_TEST_CASE(test_DumpVectors)
+
+
 BOOST_AUTO_TEST_CASE(test_DumpVectors) {
+  
+  Vector3Dtest();
   
   Vector3DdocumentationTest();
   
