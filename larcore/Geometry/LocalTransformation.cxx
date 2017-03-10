44f8bfd1bb800f9139078eb493bd89352e19cb16
Gianluca Petrillo <petrillo@fnal.gov>
2017-03-10 11:44:42 -0600

    Added missing header inclusion

--- larcore/Geometry/LocalTransformation.cxx
+++ larcore/Geometry/LocalTransformation.cxx
@@ -10,6 +10,15 @@
 // our header
 #include "LocalTransformation.h"
 
+// ROOT
+#include "TGeoMatrix.h" // TGeoHMatrix
+
+// CLHEP
+#include "CLHEP/Geometry/Transform3D.h" // HepGeom::Transform3D
+#include "CLHEP/Vector/Rotation.h" // CLHEP::HepRotation
+#include "CLHEP/Vector/RotationInterfaces.h" // CLHEP::HepRep3x3
+#include "CLHEP/Vector/ThreeVector.h" // CLHEP::Hep3Vector
+
 
 //------------------------------------------------------------------------------
 // specialisations
