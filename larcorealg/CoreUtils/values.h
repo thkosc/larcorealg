1472c406fafae5af9082b3c0819b57796be26809
Gianluca Petrillo <petrillo@slac.stanford.edu>
2019-05-09 11:21:37 -0500

    Added `util::values()` to iterate through values of a container.
    In case of the supported associative containers (key -> value),
    it allows iteration across values directly.

---
+++ larcorealg/CoreUtils/values.h
@@ -0,0 +1,150 @@
+/**
+ * @file   larcorealg/CoreUtils/values.h
+ * @brief  Definition of `util::values()` and `util::const_values()`.
+ * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
+ * @date   May 8, 2019
+ * 
+ * This is a header-only library.
+ */
+
+#ifndef LARCOREALG_COREUTILS_VALUES_H
+#define LARCOREALG_COREUTILS_VALUES_H
+
+
+// LArSoft libraries
+#include "larcorealg/CoreUtils/span.h" // util::make_transformed_span()
+
+// C/C++ libraries
+#include <map>
+#include <unordered_map>
+#include <utility> // std::forward(), std::as_const()
+#include <tuple> // std::get()
+#include <cstddef> // std::size_t
+
+
+
+namespace util {
+  
+  
+  // -- BEGIN -- Transformed iterations ----------------------------------------
+  /// @name Transformed iterations
+  /// @{
+  
+  /**
+   * @brief Range-for loop helper iterating across the values of the specified
+   *        collection.
+   * @tparam Coll type of the collection to iterate through
+   * @param coll the collection to iterate through
+   * @return an object suitable for range-for loop
+   * @see `util::const_values()`
+   * 
+   * This function is in most of cases a no-operation, returning the collection
+   * just as it was specified, to be iterated on directly.
+   * In case of mapping types, though, a different object is returned and the
+   * iteration will happen to the value type of the mapping instead than on
+   * the key-value pair.
+   * 
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::map<int, float> data { { 1, 4.0F }, { 3, 12.0F }, { 2, 8.0F } };
+   * std::vector<float> values;
+   * 
+   * for (float value: util::values(data)) 
+   *   values.push_back(value);
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * will result in `values` vector being of size `3` and with values
+   * `{ 4.0F, 8.0F, 12.0F }` (the order is the one of iterating through a
+   * `std::map`).
+   * 
+   */
+  template <typename Coll>
+  decltype(auto) values(Coll&& coll);
+  
+  /**
+   * @brief Range-for loop helper iterating across the constant values of the
+   *        specified collection.
+   * @see `util::values()`
+   * 
+   * This function is equivalent to `util::values()` but the values are
+   * extracted as if the specified collection were constant.
+   */
+  template <typename Coll>
+  decltype(auto) const_values(Coll&& coll);
+  
+  
+  /// @}
+  // -- END -- Transformed iterations ------------------------------------------
+  
+  
+} // namespace util
+
+
+//==============================================================================
+//=== template implementation
+//==============================================================================
+//------------------------------------------------------------------------------
+//--- util::values()
+//------------------------------------------------------------------------------
+namespace util::details {
+  
+  //----------------------------------------------------------------------------
+  template <typename Coll, typename = void>
+  struct values_impl {
+    
+    template <typename T>
+    static constexpr decltype(auto) iterate(T&& coll) noexcept
+      { return coll; }
+    
+  }; // struct values_impl
+  
+  
+  //----------------------------------------------------------------------------
+  template <typename Map>
+  struct map_values_impl {
+    
+    static constexpr std::size_t NElement = 1U; // this is the value
+    
+    template <typename T>
+    static constexpr decltype(auto) iterate(T&& coll) noexcept
+      {
+        auto extractor = [](auto&& value) -> decltype(auto)
+          { return std::get<NElement>(value); };
+        return util::make_transformed_span(coll, extractor);
+      }
+    
+  }; // map_values_impl
+  
+  
+  //----------------------------------------------------------------------------
+  template <typename Key, typename Value, typename... Args>
+  struct values_impl<std::map<Key, Value, Args...>>
+    : map_values_impl<std::map<Key, Value, Args...>>
+  {};
+  template <typename Key, typename Value, typename... Args>
+  struct values_impl<std::unordered_map<Key, Value, Args...>>
+    : map_values_impl<std::unordered_map<Key, Value, Args...>>
+  {};
+  
+  
+  //----------------------------------------------------------------------------
+  
+} // namespace util::details
+
+
+//------------------------------------------------------------------------------
+template <typename Coll>
+decltype(auto) util::values(Coll&& coll) {
+  using coll_t = std::remove_reference_t<Coll>;
+  return details::values_impl<coll_t>::iterate(std::forward<Coll>(coll)); 
+} // util::values()
+
+
+//------------------------------------------------------------------------------
+template <typename Coll>
+decltype(auto) util::const_values(Coll&& coll)
+  { return values(std::as_const(coll)); }
+
+
+//------------------------------------------------------------------------------
+
+
+#endif // LARCOREALG_COREUTILS_VALUES_H
---
+++ test/CoreUtils/values_test.cc
@@ -0,0 +1,182 @@
+/**
+ * @file   values_test.cc
+ * @brief  Test of `util::values()`.
+ * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
+ * @date   May 9, 2019
+ * 
+ */
+
+
+// testing library
+#include "larcorealg/CoreUtils/values.h"
+
+// Boost libraries
+#define BOOST_TEST_MODULE ( values_test )
+#include <cetlib/quiet_unit_test.hpp> // BOOST_AUTO_TEST_CASE()
+#include <boost/test/test_tools.hpp> // BOOST_CHECK(), BOOST_CHECK_EQUAL()
+
+// C/C++ libraries
+#include <numeric> // std::iota()
+#include <vector>
+#include <array>
+#include <map>
+#include <utility> // std::as_const()
+#include <memory> // std::addressof()
+#include <cstddef> // std::size_t
+
+
+// -----------------------------------------------------------------------------
+void test_values() {
+  
+  // prepare the data set
+  constexpr std::size_t N = 7U;
+  
+  std::vector<float> data(N);
+  std::iota(data.begin(), data.end(), 0.0F);
+  
+  //
+  // static checks
+  //
+  // for a vector, we expect exactly the same vector object as return value:
+  static_assert(std::is_same_v<
+    decltype(util::values(data)),
+    std::add_lvalue_reference_t<std::vector<float>>
+    >
+    );
+  BOOST_CHECK_EQUAL(&util::values(data), &data);
+  
+  //
+  // read-only access
+  //
+  {
+    std::size_t i = 0U;
+    for (auto&& value: util::values(std::as_const(data))) {
+      BOOST_CHECK_EQUAL(value, data[i]);
+      BOOST_CHECK_EQUAL(std::addressof(value), std::addressof(data[i]));
+      ++i;
+    } // for
+    BOOST_CHECK_EQUAL(i, data.size());
+  }
+  
+  //
+  // read/write access
+  //
+  {
+    std::size_t i = 0U;
+    for (auto&& value: util::values(data)) {
+      BOOST_CHECK_EQUAL(value, data[i]);
+      BOOST_CHECK_EQUAL(std::addressof(value), std::addressof(data[i]));
+      ++i;
+    } // for
+    BOOST_CHECK_EQUAL(i, data.size());
+  }
+  
+} // test_values()
+
+
+void test_const_values() {
+  
+  // prepare the data set
+  constexpr std::size_t N = 7U;
+  
+  std::vector<float> data(N);
+  std::iota(data.begin(), data.end(), 0.0F);
+  
+  //
+  // static checks
+  //
+  // for a vector, we expect exactly the same vector object as return value:
+  static_assert(std::is_same_v<
+    decltype(util::const_values(data)),
+    std::add_lvalue_reference_t<std::vector<float> const>
+    >
+    );
+  BOOST_CHECK_EQUAL(&util::const_values(data), &data);
+  
+  //
+  // read-only access
+  //
+  std::size_t i = 0U;
+  for (auto&& value: util::const_values(data)) {
+    static_assert(std::is_const_v<std::remove_reference_t<decltype(value)>>);
+    BOOST_CHECK_EQUAL(value, data[i]);
+    BOOST_CHECK_EQUAL(std::addressof(value), std::addressof(data[i]));
+    ++i;
+  } // for
+  BOOST_CHECK_EQUAL(i, data.size());
+  
+} // test_const_values()
+
+
+// -----------------------------------------------------------------------------
+template<template <typename Key, typename Value, typename... Args> typename Map>
+void test_values_map() {
+  
+  // prepare the data set
+  constexpr std::size_t N = 7U;
+  constexpr double factor = 5.0;
+  
+  std::vector<int> keys(N);
+  std::iota(keys.begin(), keys.end(), 0U);
+  Map<int, float> data;
+  for (int key: keys) data.emplace(key, key * factor);
+  
+  std::size_t i = 0U;
+  for (auto&& value: util::values(data)) {
+    static_assert(std::is_same_v<decltype(value), float&>);
+    BOOST_CHECK_EQUAL(value, data[i]);
+    BOOST_CHECK_EQUAL(std::addressof(value), std::addressof(data[i]));
+    ++i;
+  } // for
+  BOOST_CHECK_EQUAL(i, data.size());
+  
+} // test_values_map()
+
+
+// -----------------------------------------------------------------------------
+void test_values_documentation() {
+  /*
+   * The promise:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::map<int, float> data { { 1, 4.0F }, { 3, 12.0F }, { 2, 8.0F } };
+   * std::vector<float> values;
+   * 
+   * for (float value: util::values(data)) 
+   *   values.push_back(value);
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * will result in `values` vector being of size `3` and with values
+   * `{ 4.0F, 8.0F, 12.0F }`
+   */
+  
+  std::map<int, float> data { { 1, 4.0F }, { 3, 12.0F }, { 2, 8.0F } };
+  std::vector<float> values;
+  
+  for (float value: util::values(data)) 
+    values.push_back(value);
+  
+  // the test
+  BOOST_CHECK_EQUAL(values.size(), 3U);
+  BOOST_CHECK_EQUAL(values[0],  4.0F);
+  BOOST_CHECK_EQUAL(values[1],  8.0F);
+  BOOST_CHECK_EQUAL(values[2], 12.0F);
+  
+} // test_values_documentation()
+
+
+// -----------------------------------------------------------------------------
+// BEGIN Test cases  -----------------------------------------------------------
+// -----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(values_testcase) {
+  
+  test_values();
+  test_const_values();
+  test_values_map<std::map>();
+  
+  test_values_documentation();
+  
+} // BOOST_AUTO_TEST_CASE(values_testcase)
+
+
+// -----------------------------------------------------------------------------
+// END Test cases  -------------------------------------------------------------
+// -----------------------------------------------------------------------------
