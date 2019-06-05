b04602186f11237abaca68d163c556072e156c72
Gianluca Petrillo <petrillo@slac.stanford.edu>
2019-06-05 13:09:48 -0500

    Added simple operations for use in C++ STL algorithms.

---
+++ larcorealg/CoreUtils/operations.h
@@ -0,0 +1,133 @@
+/**
+ * @file   larcorealg/CoreUtils/operations.h
+ * @brief  Provides a few simple operations for use in generic programming.
+ * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
+ * @date   June 5, 2019
+ * 
+ * This is a header-only library.
+ */
+
+#ifndef LARCOREALG_COREUTILS_OPERATIONS_H
+#define LARCOREALG_COREUTILS_OPERATIONS_H
+
+
+// C++ standard library
+#include <memory> // std::addressof()
+
+
+namespace util {
+  
+  //----------------------------------------------------------------------------
+  /**
+   * @brief Functor returning the address in memory of the operand.
+   * @see `util::takeAddress()`
+   * 
+   * Example:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int*> ptrs(data.size());
+   * std::transform
+   *   (data.begin(), data.end(), ptrs.begin(), util::AddressTaker{});
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * will fill the vector `ptrs` with pointers to the elements of `data`.
+   * 
+   * @note The address is extracted via `std::addressof()` and it bypasses the
+   *       `operator&()` of the operand.
+   */
+  struct AddressTaker {
+    
+    /// Returns the address of the argument.
+    template <typename T>
+    auto operator() (T& ref) const { return std::addressof(ref); }
+    
+  }; // struct AddressTaker
+  
+  
+  
+  /**
+   * @brief Returns a functor that returns the address of its argument.
+   * @see `util::AddressTaker`
+   * 
+   * Example:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int*> ptrs(data.size());
+   * std::transform
+   *   (data.begin(), data.end(), ptrs.begin(), util::takeAddress());
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * will fill the vector `ptrs` with pointers to the elements of `data`.
+   * 
+   * Why bother?
+   * ------------
+   * 
+   * C++ already provides a tool to effectively take an address,
+   * `std::addressof`. The reason for `takeAddress()` is that `std::addressof()`
+   * is a function, with many overloads, and to use it in a STL algorithm the
+   * overload has to be resolved first. For example:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * using addressof_t = int const*(*)(int const&);
+   * 
+   * std::transform(data.cbegin(), data.cend(), std::back_inserter(dataPtr),
+   *   ((addressof_t) &std::addressof));
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * One important limit is that the type of the argument (in this case
+   * `int const&`) needs to be known or deduced in a quite precise way, in
+   * particular regarding constantness and referenceness.
+   * This is unconvenient and convoluted enough that one would rather create
+   * a new function, like:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * auto takeAddress = [](auto&& ref){ return std::addressof(ref); };
+   * 
+   * std::vector<int const*> dataPtr;
+   * std::transform(data.cbegin(), data.cend(), std::back_inserter(dataPtr),
+   *   takeAddress);
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * This `util::takeAddress()` operates in a very similar way to the lambda in
+   * the last example.
+   */
+  auto takeAddress() { return AddressTaker(); }
+  
+  
+  //----------------------------------------------------------------------------
+  /**
+   * @brief Functor dereferencing the operand.
+   * @see `util::dereference()`
+   * 
+   * Example:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int> values(ptrs.size());
+   * std::transform
+   *   (ptrs.cbegin(), ptrs.cend(), values.begin(), util::Dereferencer{});
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * will fill the vector `values` with the values pointed by the elements in
+   * `ptrs`.
+   */
+  struct Dereferencer {
+    
+    /// Returns `*ptr`.
+    template <typename T>
+    auto operator() (T&& ptr) const { return *ptr; }
+    
+  }; // struct Dereferencer
+  
+  
+  /**
+   * @brief Returns a functor that returns `*ptr` of its argument `ptr`.
+   * @see `util::Dereferencer`
+   * 
+   * Example:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int> values(ptrs.size());
+   * std::transform
+   *   (ptrs.cbegin(), ptrs.cend(), values.begin(), util::dereference());
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * will fill the vector `values` with the values pointed by the elements in
+   * `ptrs`.
+   */
+  decltype(auto) dereference() { return Dereferencer(); }
+  
+  
+  //----------------------------------------------------------------------------
+  
+} // namespace util
+
+
+#endif // LARCOREALG_COREUTILS_OPERATIONS_H
---
+++ test/CoreUtils/operations_test.cc
@@ -0,0 +1,274 @@
+/**
+ * @file   larcorealg/test/CoreUtils/operations_test.cc
+ * @brief  Unit test for `larcorealg/CoreUtils/operations.h`.
+ * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
+ * @date   June 5, 2019
+ * @see    `larcorealg/CoreUtils/operations.h`
+ */
+
+
+// Boost libraries
+#define BOOST_TEST_MODULE ( operations_test )
+#include <cetlib/quiet_unit_test.hpp> // BOOST_AUTO_TEST_CASE()
+#include <boost/test/test_tools.hpp> // BOOST_CHECK(), BOOST_CHECK_EQUAL()
+
+// LArSoft libraries
+#include "larcorealg/CoreUtils/operations.h"
+#include "larcorealg/CoreUtils/zip.h"
+
+// C/C++ standard libraries
+#include <vector>
+#include <algorithm>
+#include <numeric> // std::iota()
+
+
+
+//------------------------------------------------------------------------------
+void test_AddressTaker_documentation() {
+  
+  std::vector<int> data(10U);
+  std::iota(data.begin(), data.end(), 0U);
+  
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  /*
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int*> ptrs(data.size());
+   * std::transform
+   *   (data.begin(), data.end(), ptrs.begin(), util::AddressTaker{});
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   */
+  
+  std::vector<int*> ptrs(data.size());
+  std::transform
+    (data.begin(), data.end(), ptrs.begin(), util::AddressTaker{});
+  
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // the test
+  BOOST_CHECK_EQUAL(ptrs.size(), data.size());
+  for (auto&& [ value, ptr ]: util::zip(data, ptrs)) {
+    
+    BOOST_CHECK(ptr);
+    BOOST_CHECK_EQUAL(*ptr, value);
+    BOOST_CHECK_EQUAL(ptr, &value);
+    
+  } // for
+  
+  
+} // test_AddressTaker_documentation()
+
+  
+//------------------------------------------------------------------------------
+void test_takeAddress_documentation() {
+  
+  std::vector<int> data(10U);
+  std::iota(data.begin(), data.end(), 0U);
+  
+  /*
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int*> ptrs(data.size());
+   * std::transform
+   *   (data.begin(), data.end(), ptrs.begin(), util::takeAddress());
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   */
+  
+  std::vector<int*> ptrs(data.size());
+  std::transform
+    (data.begin(), data.end(), ptrs.begin(), util::takeAddress());
+  
+  // the test
+  BOOST_CHECK_EQUAL(ptrs.size(), data.size());
+  for (auto&& [ value, ptr ]: util::zip(data, ptrs)) {
+    
+    BOOST_CHECK(ptr);
+    BOOST_CHECK_EQUAL(*ptr, value);
+    BOOST_CHECK_EQUAL(ptr, &value);
+    
+  } // for
+  
+  
+} // test_takeAddress_documentation()
+
+  
+//------------------------------------------------------------------------------
+void test_takeAddress() {
+  
+  std::vector<int> data(10U);
+  std::iota(data.begin(), data.end(), 0U);
+  
+  std::vector<int const*> dataPtr;
+  std::transform(data.cbegin(), data.cend(), std::back_inserter(dataPtr),
+    util::takeAddress());
+  
+  for (auto&& [ value, ptr ]: util::zip(data, dataPtr)) {
+    
+    BOOST_CHECK(ptr);
+    BOOST_CHECK_EQUAL(*ptr, value);
+    BOOST_CHECK_EQUAL(ptr, &value);
+    
+  } // for
+  
+} // test_takeAddress()
+
+
+//------------------------------------------------------------------------------
+void test_takeAddress_whyBother() {
+  
+  std::vector<int> data(10U);
+  std::iota(data.begin(), data.end(), 0U);
+  
+  std::vector<int const*> dataPtr;
+  
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // `std::addressof()` approach
+  using addressof_t = int const*(*)(int const&);
+  
+  std::transform(data.cbegin(), data.cend(), std::back_inserter(dataPtr),
+    ((addressof_t) &std::addressof));
+  
+  for (auto&& [ value, ptr ]: util::zip(data, dataPtr)) {
+    
+    BOOST_CHECK(ptr);
+    BOOST_CHECK_EQUAL(*ptr, value);
+    BOOST_CHECK_EQUAL(ptr, &value);
+    
+  } // for
+  
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // lambda approach
+  dataPtr.clear();
+  
+  auto takeAddress = [](auto&& ref){ return std::addressof(ref); };
+  
+  std::transform(data.cbegin(), data.cend(), std::back_inserter(dataPtr),
+    takeAddress);
+  
+  for (auto&& [ value, ptr ]: util::zip(data, dataPtr)) {
+    
+    BOOST_CHECK(ptr);
+    BOOST_CHECK_EQUAL(*ptr, value);
+    BOOST_CHECK_EQUAL(ptr, &value);
+    
+  } // for
+  
+} // test_takeAddress_whyBother()
+
+
+//------------------------------------------------------------------------------
+void test_Dereferencer_documentation() {
+  
+  std::vector<int> data(10U);
+  std::iota(data.begin(), data.end(), 0U);
+  
+  std::vector<int*> ptrs(data.size());
+  std::transform
+    (data.begin(), data.end(), ptrs.begin(), util::takeAddress());
+  
+  /*
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int> values(ptrs.size());
+   * std::transform
+   *   (ptrs.cbegin(), ptrs.cend(), values.begin(), util::Dereferencer{});
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   */
+  
+  std::vector<int> values(ptrs.size());
+  std::transform
+    (ptrs.cbegin(), ptrs.cend(), values.begin(), util::Dereferencer{});
+  
+  // the test
+  BOOST_CHECK_EQUAL(values.size(), data.size());
+  for (auto&& [ value, orig ]: util::zip(data, values)) {
+    
+    BOOST_CHECK_EQUAL(value, orig);
+    
+  } // for
+  
+} // test_Dereferencer_documentation()
+
+  
+//------------------------------------------------------------------------------
+void test_dereference_documentation() {
+  
+  std::vector<int> data(10U);
+  std::iota(data.begin(), data.end(), 0U);
+  
+  std::vector<int*> ptrs(data.size());
+  std::transform
+    (data.begin(), data.end(), ptrs.begin(), util::takeAddress());
+  
+  /*
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * std::vector<int> values(ptrs.size());
+   * std::transform
+   *   (ptrs.cbegin(), ptrs.cend(), values.begin(), util::dereference());
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   */
+  
+  std::vector<int> values(ptrs.size());
+  std::transform
+    (ptrs.cbegin(), ptrs.cend(), values.begin(), util::dereference());
+  
+  // the test
+  BOOST_CHECK_EQUAL(values.size(), data.size());
+  for (auto&& [ value, orig ]: util::zip(data, values)) {
+    
+    BOOST_CHECK_EQUAL(value, orig);
+    
+  } // for
+  
+  
+} // test_dereference_documentation()
+
+  
+//------------------------------------------------------------------------------
+void test_dereference() {
+  
+  std::vector<int> data(10U);
+  std::iota(data.begin(), data.end(), 0U);
+  
+  std::vector<int const*> dataPtrs;
+  std::transform(data.cbegin(), data.cend(), std::back_inserter(dataPtrs),
+    util::takeAddress());
+  
+  std::vector<int> dataAgain;
+  std::transform(
+    dataPtrs.cbegin(), dataPtrs.cend(),
+    std::back_inserter(dataAgain),
+    util::dereference()
+    );
+  
+  BOOST_CHECK_EQUAL(dataAgain.size(), data.size());
+  for (auto&& [ value, valueAgain ]: util::zip(data, dataAgain)) {
+    
+    BOOST_CHECK_EQUAL(valueAgain, value);
+    BOOST_CHECK_NE(&valueAgain, &value);
+    
+  } // for
+  
+} // test_dereference()
+
+
+
+
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(takeAddress_testcase) {
+
+  test_takeAddress();
+  test_takeAddress_documentation();
+  test_AddressTaker_documentation();
+  test_takeAddress_whyBother();
+
+} // BOOST_AUTO_TEST_CASE(takeAddress_testcase)
+
+
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(dereference_testcase) {
+
+  test_dereference();
+  test_dereference_documentation();
+  test_Dereferencer_documentation();
+
+} // BOOST_AUTO_TEST_CASE(dereference_testcase)
+
+
+//------------------------------------------------------------------------------
