b4f38a9ce9a8bbf80790ab9d0a6e45da96c0ed9a
Chris Green <greenc@fnal.gov>
2022-04-26 09:42:45 -0500

    Quiet header no longer necessary for ROOT's Math/Functor.h

--- larcorealg/CoreUtils/quiet_Math_Functor.h
+++ larcorealg/CoreUtils/quiet_Math_Functor.h
@@ -1,15 +1,2 @@
-/**
- * This is a wrapper to ROOT's Math/Functor.h that disables a warning about deprecated stuff.
- * That header is known to use std::auto_ptr.
- *
- * A unit test is connected with this header.
- * When that unit test fails, it means the workaround is not needed any more.
- * Then, this wrapper should be removed and all the include directives restored.
- */
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
+#pragma message "wrapper header no longer necessary since ROOT 6.10/04"
 #include "Math/Functor.h"
-
-#pragma GCC diagnostic pop
