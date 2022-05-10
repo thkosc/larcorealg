e094f44712f410ca9e2c6a5a717c4091f291b4fe
Chris Green <greenc@fnal.gov>
2022-05-10 13:34:33 -0500

    Make message more informative

--- larcorealg/CoreUtils/quiet_Math_Functor.h
+++ larcorealg/CoreUtils/quiet_Math_Functor.h
@@ -1,2 +1,3 @@
-#pragma message "wrapper header no longer necessary since ROOT 6.10/04"
+#pragma message "wrapper header no longer necessary since ROOT 6.10/04 - include Math/Functor.h directly"
+
 #include "Math/Functor.h"
